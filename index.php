6e1bfb7ba137cb4903e6d2987276bb7dde783b7a
sahida-akter <mssahidasmrity@gmail.com>
2023-03-25 14:24:37 +0600

    upload done

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+#function 01
+include_once "./function.php";
+info();
+echo "<hr>";
+
+#function 02
+devs_info('Mst. Sahida Akter', 'Web developer', 24);
+echo "<hr>";
+devs_info('Asraf', 'Mentor of coderstrust Bangladeesh', 40);
+echo "<hr>";
+devs_info('Mina', 'Android app developer', 25);
+echo "<hr>";
+devs_info('Tina', 'Dekstop Application developer', 30);
+echo "<hr>";
+
+
+#function 03
+
+teachers_info ('UITS', 'Machine Learrning Teacher', 2020);
+echo "<hr>";
+teachers_info ('AUST', 'Artificial Intelligence Teacher', 2010);
+echo "<hr>";
+teachers_info ('NSU', ' Operating System Teacher', 2000);
+echo "<hr>";
+
+# return function 04
+agecal ('Smrity', 1998);
+echo "<hr>";
+
+# function 05
+hat('u', 'This is uppercase');
+echo "<hr>";
+hat('l', 'This is lowerrcase');
+
+?>
